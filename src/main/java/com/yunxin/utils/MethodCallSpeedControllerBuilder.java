5fbd907077a29cdd3a224d3c2ce5ba518c4b60af
hedingwei <56910061@qq.com>
2020-07-03 16:04:57 +0800

    ~~~

---
+++ src/main/java/com/yunxin/utils/MethodCallSpeedControllerBuilder.java
@@ -0,0 +1,165 @@
+package com.yunxin.utils;
+
+import lombok.SneakyThrows;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class MethodCallSpeedControllerBuilder<T> {
+
+
+    private Object originObject = null;
+
+    private Class<T> methodInterface = null;
+
+    public MethodCallSpeedControllerBuilder(Object originObject, Class<T> methodInterface) {
+        this.originObject = originObject;
+        this.methodInterface = methodInterface;
+    }
+
+    public T build(int speed){
+        if(SpeedControllerHandler.configurationInstance.containsKey(methodInterface)){
+            SpeedControllerHandler.SpeedControllerConfiguration config = SpeedControllerHandler.configurationInstance.get(methodInterface);
+            config.speed = speed;
+        }else{
+            SpeedControllerHandler.SpeedControllerConfiguration config = new SpeedControllerHandler.SpeedControllerConfiguration(speed);
+            SpeedControllerHandler.configurationInstance.put(methodInterface,config);
+        }
+        return (T) Proxy.newProxyInstance(
+                originObject.getClass().getClassLoader(),
+                new Class[]{methodInterface},
+                new SpeedControllerHandler(originObject, SpeedControllerHandler.configurationInstance.get(methodInterface))
+        );
+    }
+
+
+    public static <T> T build(Object o, Class<T> tInterface, int speed){
+        if(SpeedControllerHandler.configurationInstance.containsKey(tInterface)){
+            SpeedControllerHandler.SpeedControllerConfiguration config = SpeedControllerHandler.configurationInstance.get(tInterface);
+            config.speed = speed;
+        }else{
+            SpeedControllerHandler.SpeedControllerConfiguration config = new SpeedControllerHandler.SpeedControllerConfiguration(speed);
+            SpeedControllerHandler.configurationInstance.put(tInterface,config);
+        }
+
+        return (T) Proxy.newProxyInstance(
+                o.getClass().getClassLoader(),
+                new Class[]{tInterface},
+                new SpeedControllerHandler(o, SpeedControllerHandler.configurationInstance.get(tInterface))
+        );
+
+
+    }
+
+
+
+
+    public static class SpeedControllerHandler implements InvocationHandler {
+
+        public static Map<Class, SpeedControllerHandler.SpeedControllerConfiguration> configurationInstance = new HashMap<>();
+
+
+        SpeedControllerHandler.SpeedControllerConfiguration controllerConfiguration;
+
+        private Object object;
+
+
+        public SpeedControllerHandler(Object object, SpeedControllerHandler.SpeedControllerConfiguration controllerConfiguration) {
+            this.object = object;
+            this.controllerConfiguration = controllerConfiguration;
+        }
+
+
+
+        @SneakyThrows
+        public  long ajust(){
+            long current = System.currentTimeMillis()/1000;
+            synchronized (controllerConfiguration.map){
+                if(controllerConfiguration.map.containsKey(current)){
+                    AtomicLong l = controllerConfiguration.map.get(current);
+                    long size = l.addAndGet(1);
+                    if(size>controllerConfiguration.speed){
+                        Thread.sleep(1000);
+                        return ajust();
+                    }
+
+                }else{
+                    controllerConfiguration.map.put(current,new AtomicLong(0));
+                    return ajust();
+
+                }
+            }
+
+
+            return current;
+        }
+
+
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+            synchronized (controllerConfiguration.counter){
+                long id= controllerConfiguration.counter.addAndGet(1);
+                long current = ajust();
+                if(id%100==0){
+                    System.out.println("["+id+"]+c["+current+"]->"+controllerConfiguration.map.get(current)+ " total: "+controllerConfiguration.counter.get() );
+                }
+
+            }
+
+
+
+
+
+            Object o = null;
+            o = method.invoke(this.object,args);
+//            synchronized (this){
+//
+//            }
+
+            return o;
+        }
+
+        public static class SpeedControllerConfiguration{
+
+            public SpeedControllerConfiguration(float speed) {
+                this.speed = speed;
+            }
+
+            private  float speed = 1000;
+
+            private  AtomicLong counter = new AtomicLong(0);
+
+            private  Map<Long,AtomicLong> map = new HashMap<>();
+
+            public float getSpeed() {
+                return speed;
+            }
+
+            public void setSpeed(float speed) {
+                this.speed = speed;
+            }
+
+            public AtomicLong getCounter() {
+                return counter;
+            }
+
+            public void setCounter(AtomicLong counter) {
+                this.counter = counter;
+            }
+
+            public Map<Long, AtomicLong> getMap() {
+                return map;
+            }
+
+            public void setMap(Map<Long, AtomicLong> map) {
+                this.map = map;
+            }
+        }
+    }
+}
--- src/main/java/com/yunxin/utils/Test.java
+++ src/main/java/com/yunxin/utils/Test.java
@@ -1,25 +1,75 @@
 package com.yunxin.utils;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Test implements ITest {
 
+    static long startTime = 0;
     public static void main(String[] args) throws Throwable {
 
 
-        byte[] d = Work.Bytes.random(4);
-        short[] s = new short[d.length];
-        for(int i=0;i<s.length;i++){
-            s[i] = Work.Bytes.ubyte(d[i]);
-        }
-        Arrays.sort(s);
 
-        for(int i=0;i<s.length;i++){
-            System.out.println(s[i]);
+        ParallelJob job = new ParallelJob(500, new ParallelJob.TaskDoneListener() {
+            @Override
+            public void onTaskDone(ParallelJob job) {
+                System.out.println("all taskDone in: "+ (System.currentTimeMillis() - startTime));
+            }
+        });
+
+
+        for(int i=0;i<1000;i++){
+            job.submitSubTask(new ChatRoomTask(i,2));
         }
+        startTime = System.currentTimeMillis();
+        job.start();
 
 
 
 
     }
+
+    public static class ChatRoomTask implements Runnable
+    {
+        private int chatRoomId = 0;
+        private int aFinalTaskSize = 10;
+        private List<ITask> taskList = new ArrayList<>();
+
+        public ChatRoomTask(int chatRoomId, int aFinalTaskSize) {
+            this.chatRoomId = chatRoomId;
+            this.aFinalTaskSize = aFinalTaskSize;
+            for(int i=0;i<aFinalTaskSize;i++){
+//                taskList.add(new AFinalTask());
+
+                taskList.add(MethodCallSpeedControllerBuilder.build(new SendingTask(),ITask.class,10));
+            }
+
+        }
+
+        @Override
+        public void run() {
+            for(int i=0;i<taskList.size();i++){
+                taskList.get(i).doTask();
+            }
+        }
+    }
+
+    public static class SendingTask implements ITask{
+
+        public void doTask(){
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static interface ITask{
+        public void doTask();
+    }
+
+
+
+
 }
